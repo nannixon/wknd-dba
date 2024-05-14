b099f1f8ff0c627357c702f495a0c57cb537689a
Tucker Elliott <telliott@adobe.com>
2024-05-14 12:00:01 -0400

    wknd offer

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -2,27 +2,27 @@
 export default async function decorate(block) {
   const aempublishurl = 'https://publish-p99452-e916802.adobeaemcloud.com';
   const aemauthorurl = 'https://author-p99452-e916802.adobeaemcloud.com';
-  const persistedquery = '/graphql/execute.json/securbank/OfferByPath';
-  const offerpath = '/content/dam/securbank/en/offers/997';
+  const persistedquery = '/graphql/execute.json/aem-demo-assets/native-app-adventure';
+  // const offerpath = '/content/dam/aem-demo-assets/en/adventures/bali-surf-camp/bali-surf-camp';
+  const variationname = 'main';
+  const offerpath = block.querySelector(':scope div:nth-child(1) > div').innerHTML.trim();
   // const offerpath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
-  const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML.trim();
+  // const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML.trim();
 
-  /*
+  
   const url = window.location && window.location.origin && window.location.origin.includes('author')
-    ? `${aemauthorurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`
-    : `${aempublishurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`;
+    ? `${aemauthorurl}${persistedquery};apath=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`
+    : `${aempublishurl}${persistedquery};apath=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`;
   const options = { credentials: 'include' };
-*/
-const url = 'https://publish-p99452-e916802.adobeaemcloud.com/graphql/execute.json/securbank/OfferByPath;path=/content/dam/securbank/en/offers/997;variation=main'
 
-  console.log(url); //https://author-p123917-e1220159.adobeaemcloud.com/graphql/execute.json/securbank/OfferByPath;path=/content/dam/securbank/en/offers/997;variation=main;ts=172.03956935404463
+  console.log(url);
 
   const cfReq = await fetch(url, options)
     .then((response) => response.json())
     .then((contentfragment) => {
       let offer = '';
       if (contentfragment.data) {
-        offer = contentfragment.data.offerByPath.item;
+        offer = contentfragment.data.adventureByPath.item;
       }
       return offer;
     });
@@ -31,10 +31,9 @@ const url = 'https://publish-p99452-e916802.adobeaemcloud.com/graphql/execute.js
 
   block.innerHTML = `
   <div class='banner-content' data-aue-resource=${itemId} data-aue-type="reference" data-aue-filter="cf">
-      <div data-aue-prop="heroImage" data-aue-type="media" class='banner-detail' style="background-image: linear-gradient(90deg,rgba(0,0,0,0.6), rgba(0,0,0,0.1) 80%) ,url(${aempublishurl + cfReq.heroImage._dynamicUrl});">
-          <p data-aue-prop="headline" data-aue-type="text" class='pretitle'>${cfReq.headline}</p>
-          <p data-aue-prop="pretitle" data-aue-type="text" class='headline'>${cfReq.pretitle}</p>
-          <p data-aue-prop="detail" data-aue-type="richtext" class='detail'>${cfReq.detail.plaintext}</p>
+      <div data-aue-prop="heroImage" data-aue-type="media" class='banner-detail' style="background-image: linear-gradient(90deg,rgba(0,0,0,0.6), rgba(0,0,0,0.1) 80%) ,url(${cfReq.primaryImage._publishUrl});">
+          <p data-aue-prop="headline" data-aue-type="text" class='pretitle'>${cfReq.title}</p>
+          <p data-aue-prop="detail" data-aue-type="richtext" class='detail'>${cfReq.description.plaintext}</p>
       </div>
       <div class='banner-logo'>
       </div>
